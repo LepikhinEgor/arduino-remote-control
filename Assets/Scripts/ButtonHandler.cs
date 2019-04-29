6a65989a15163924866c609c234464679d482441
Egor Lepikhin <lepikhin.egor@yandex.ru>
2019-04-29 12:40:35 +0300

    now message-buttons movable and pressable

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ButtonHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+public class ButtonHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+
 {
+    private float pressTimer = 0;
+    private Vector3 clickOffset;
+    private Vector3 oldPosition;
+
+    bool isMoving = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +21,12 @@ public class ButtonHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     // Update is called once per frame
     void Update()
     {
-        
+        if (isMoving)
+        {
+            transform.position = Input.mousePosition - clickOffset;
+
+            pressTimer += Time.deltaTime;
+        }
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -26,6 +37,11 @@ public class ButtonHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
         //for (int i = 0; i < message.Length; i++)
        //     Debug.Log(message[i]);
         BluetoothController.SetMessage(message);
+        isMoving = true;
+
+        oldPosition = transform.position;
+
+        clickOffset = Input.mousePosition - transform.position;
     }
 
     private byte[] StringToByteArray(string str)
@@ -43,12 +59,26 @@ public class ButtonHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
 
     public void OnPointerUp(PointerEventData eventData)
     {
-       // Debug.Log("button unpressed");
+        Debug.Log("button unpressed");
         BluetoothController.SetMessage(null);
+        isMoving = false;
+        Debug.Log("timer " + pressTimer);
+
+        Vector3 moveOffset = oldPosition - transform.position;
+        Debug.Log(moveOffset.magnitude);
+
+        if (pressTimer < 0.3 && Mathf.Abs(moveOffset.magnitude) < 0.001F)
+        {
+            ButtonClickAction();
+        }
+
+        pressTimer = 0;
     }
 
-    public void OnPointerClick(PointerEventData eventData)
+
+    private void ButtonClickAction()
     {
+        Debug.Log("button clicked");
         if (!AppAction.isEditButtonDataMode)
         {
             if (AppAction.selectedButton == this.gameObject)
@@ -67,4 +97,25 @@ public class ButtonHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
             }
         }
     }
+    /*public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("button clicked");
+        if (!AppAction.isEditButtonDataMode)
+        {
+            if (AppAction.selectedButton == this.gameObject)
+            {
+                AppAction.selectedButton = null;
+                Destroy(this.gameObject.GetComponent<Outline>());
+            }
+            else
+            {
+                if (AppAction.selectedButton != null)
+                    Destroy(AppAction.selectedButton.GetComponent<Outline>());
+                AppAction.selectedButton = this.gameObject;
+                this.gameObject.AddComponent<Outline>();
+                this.gameObject.GetComponent<Outline>().effectColor = Color.yellow;
+                this.gameObject.GetComponent<Outline>().effectDistance = new Vector2(3, 3);
+            }
+        }
+    }*/
 }
