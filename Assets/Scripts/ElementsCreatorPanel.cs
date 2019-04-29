bb548fbf87b16cbb8fed78fa57f53eee107862e9
Egor Lepikhin <lepikhin.egor@yandex.ru>
2019-04-29 11:07:10 +0300

    add feature for hide buttons panel

---
+++ Assets/Scripts/ElementsCreatorPanel.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementsCreatorPanel : MonoBehaviour
+{
+    private Vector3 hidenInventoryPos;
+    private Vector3 defaultInventoryPos;
+
+    private bool isHideAnimation;
+    private bool isShowAnimation;
+
+    private float bottomAttachButtonHeight;
+
+    private float panelHeight;
+
+    public void ShowPanel()
+    {
+        isShowAnimation = true;
+    }
+
+    public void HidePanel()
+    {
+        isHideAnimation = true;
+    }
+
+    public void SetBottomAttachButtonHeight(float height)
+    {
+        bottomAttachButtonHeight = height;
+        FindInventoryPositions();
+    }
+
+    public float GetPanelHeight()
+    {
+        return this.panelHeight;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindInventoryPositions();
+    }
+
+    private void FindInventoryPositions()
+    {
+        //высота кнопки в процентах
+        panelHeight = transform.GetComponent<RectTransform>().anchorMax.y - GetComponent<RectTransform>().anchorMin.y;
+        // в пикселях
+        panelHeight *= Screen.height;
+
+        defaultInventoryPos = transform.position;
+
+        Vector3 pos = transform.position;
+        pos.y += panelHeight;
+        Debug.Log(bottomAttachButtonHeight + " " + panelHeight);
+        hidenInventoryPos = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHideAnimation)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, hidenInventoryPos, 500 * Time.deltaTime);
+
+            if (transform.position.y == hidenInventoryPos.y)
+                isHideAnimation = false;
+        }
+
+        if (isShowAnimation)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, defaultInventoryPos, 500 * Time.deltaTime);
+
+            if (transform.position.y == defaultInventoryPos.y)
+            {
+                isShowAnimation = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ToUserModeButtonHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToUserModeButtonHandler : MonoBehaviour
+{
+    GameObject creatorPanel;
+
+    private bool isMoving;
+    private Vector3 attachPanelPosition;
+
+    private float buttonHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        creatorPanel = GameObject.FindGameObjectWithTag("ButtonsCreator");
+        this.gameObject.GetComponent<Button>().onClick.AddListener(ToUserMode);
+
+        buttonHeight = GetComponent<RectTransform>().anchorMax.y - GetComponent<RectTransform>().anchorMin.y;
+        // в пикселях
+        float parentHeigth  = (transform.parent.GetComponent<RectTransform>().anchorMax.y - transform.parent.GetComponent<RectTransform>().anchorMin.y)*Screen.height;
+        buttonHeight *= parentHeigth;
+        Debug.Log(buttonHeight);
+    }
+
+    public void ToUserMode()
+    {
+        creatorPanel.GetComponent<ElementsCreatorPanel>().SetBottomAttachButtonHeight(buttonHeight);
+        creatorPanel.GetComponent<ElementsCreatorPanel>().HidePanel();
+        isMoving = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (isMoving)
+        {
+            float panelHeight = creatorPanel.GetComponent<ElementsCreatorPanel>().GetPanelHeight();
+            attachPanelPosition = creatorPanel.transform.position;
+            attachPanelPosition.y -= panelHeight / 2 + buttonHeight / 2;
+            transform.position =  attachPanelPosition;
+
+        }*/
+    }
+}
